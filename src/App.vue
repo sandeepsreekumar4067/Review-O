3e3eab07e8ec40cc41011e622acd3a23bf4bc872
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-07-18 11:56:49 +0530

    initial commit/ login page done

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <LoginPage/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import "../src/style/app.css"
+import LoginPage from "./pages/LoginPage.vue";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LoginPage
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="login-container">
+    <div class="left-container">
+      <img :src="LoginImage" alt="" />
+      <span> Sed ut perspiciatis unde omnis iste natus </span>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididun.
+      </p>
+    </div>
+    <div class="right-container">
+      <div class="logo-container">
+        <img :src="Logo" />
+      </div>
+      <div class="login-title">
+        Welcome back to the <br />
+        officeO community
+      </div>
+      <div class="login-buttons">
+        <div class="google">
+          <img :src="google" alt="">
+          <p>Log In with Google</p>
+        </div>
+        <div class="facebook">
+          <img :src="facebook" alt="">
+          <p>Log In with Facebook</p>
+        </div>
+      </div>
+      <div class="login-input-container">
+        <input type="text" placeholder="Email Address" class="email-input"/>
+        <div class="password-container">
+          <input :type="passVisibility?'text':'password'" placeholder="Password" id="password-input" />
+          <i class="bi bi-eye-slash" id="toggle-password" v-if="!passVisibility" @click="togglePassword"></i>
+          <i class="bi bi-eye" v-else @click="togglePassword"></i>
+        </div>
+      </div>
+      <div class="login-submit-container">
+        <div class="remember">
+          <input type="checkbox" name="rememberME" id="">
+          <label for="rememberME">Remember me</label>
+        </div>
+        <input type="button" value="Login">
+      </div>
+      <div class="create-account-container">
+        <p>No Account Yet ?</p>
+        <a href="">Sign Up</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "../style/LoginPage.css";
+import "../style/app.css";
+import LoginImage from "../assets/Illustration.png";
+import Logo from "../assets/Logo2.png";
+import google from '../assets/google.svg';
+import facebook from '../assets/facebook.svg'
+export default {
+  name: "LoginPage",
+  data() {
+    return {
+      LoginImage: LoginImage,
+      Logo: Logo,
+      passVisibility:false,
+      passType:"password",
+      google:google,
+      facebook:facebook
+    };
+  },
+  methods:{
+    togglePassword(){
+      this.passVisibility = !this.passVisibility
+
+    }
+  }
+};
+</script>
+
+
+
+<style>
+</style>
