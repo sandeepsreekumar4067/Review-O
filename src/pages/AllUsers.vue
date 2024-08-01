b1bebd68872a4077af75575267d5ef7a308b2ca0
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-08-01 22:11:26 +0530

    made the all users menu

--- src/pages/AllUsers.vue
+++ src/pages/AllUsers.vue
@@ -1,10 +1,33 @@
 <template>
-  helo
+  <div class="allUsers-container">
+    <NavBar :title="title" :subtitle="subtitle" :user="user" @toggle-sidebar="setSidebarActive"/>
+    <div :class="['allUsers-components',{active:isSidebarActive}]">
+      <SideBar :isActive="isSidebarActive" :activeSpan="3"/>
+      All Users
+    </div>
+  </div>
 </template>
 
 <script>
+import NavBar from '@/components/NavBar.vue';
+import '../style/allusers.css'
+import SideBar from '@/components/SideBar.vue';
 export default {
-    name:'AllUsers'
+    name:'AllUsers',
+    components :{NavBar,SideBar},
+    data(){
+      return{
+        title:'All Users',
+        subtitle:'All Users Information',
+        user:'Sandeep Sreekumar',
+        isSidebarActive:true
+      }
+    },
+    methods:{
+      setSidebarActive(){
+        this.isSidebarActive=!this.isSidebarActive
+      }
+    }
 }
 </script>
 
