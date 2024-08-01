d82ae0515db87490febec668aa8021574a3a3f27
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-08-01 21:50:51 +0530

    minor changes

--- src/pages/DashBoard.vue
+++ src/pages/DashBoard.vue
@@ -1,9 +1,31 @@
 <template>
-  helo
+  <div class="dashBoard-container">
+    <NavBar :title="title" :subtitle="subtitle" :user="user" @toggle-sidebar="setSidebarActive"/>
+    <div class="dashBoard-components">
+        <SideBar :isActive="isSidebarActive" :activeSpan="1"/>
+    </div>
+  </div>
 </template>
 
 <script>
+import NavBar from '@/components/NavBar.vue';
+import '../style/dashboard.css'
+import SideBar from '@/components/SideBar.vue';
 export default {
-    name:'DashBoard'
+    name:'DashBoard',
+    components:{NavBar,SideBar},
+    data(){
+        return{
+            isSidebarActive:true,
+            title:'Dash Board',
+            subtitle:'Dashboard info',
+            user:'Sandeep Sreekumar'
+        }
+    },
+    methods:{
+        setSidebarActive(){
+            this.isSidebarActive=!this.isSidebarActive
+        }
+    }
 }
 </script>
