c53b10f5b5bba453ea2a7230902fb942fcdb2467
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-08-06 10:36:49 +0530

    added the user title elements

--- src/pages/AllUsers.vue
+++ src/pages/AllUsers.vue
@@ -1,51 +1,65 @@
 <template>
   <div class="allUsers-container">
-    <NavBar :title="title" :subtitle="subtitle" :user="user" @toggle-sidebar="setSidebarActive"/>
-    <div :class="['allUsers-components',{active:isSidebarActive}]">
-      <SideBar :isActive="isSidebarActive" :activeSpan="3"/>
+    <NavBar
+      :title="title"
+      :subtitle="subtitle"
+      :user="user"
+      @toggle-sidebar="setSidebarActive"
+    />
+    <div :class="['allUsers-components', { active: isSidebarActive }]">
+      <SideBar :isActive="isSidebarActive" :activeSpan="3" />
+      <!--User search bar-->
       <div class="users-searchBar-and-addContainer">
-          <div class="users-searchBar">
-            <i class="bi bi-search"></i>
-            <input type="text" placeholder="Search" />
+        <div class="users-searchBar">
+          <i class="bi bi-search"></i>
+          <input type="text" placeholder="Search" />
+        </div>
+        <div class="users-Adding-and-filtering-container">
+          <div id="users" @click="navigate('/add-organisation')">
+            <i class="bi bi-plus-circle"></i>
+            <p>Add User</p>
           </div>
-          <div class="users-Adding-and-filtering-container">
-            <div id="users" @click="navigate('/add-organisation')">
-              <i class="bi bi-plus-circle"></i>
-              <p>Add Organisation</p>
-            </div>
-            <div id="Userfilter">
-              <i class="bi bi-sliders"></i>
-              <p>Filter</p>
-            </div>
+          <div id="Userfilter">
+            <i class="bi bi-sliders"></i>
+            <p>Filter</p>
           </div>
         </div>
+      </div>
+      <!--User Details container-->
+      <div class="user-details-container">
+        <div class="user-title">
+          <span>User name</span>
+          <span>Email address</span>
+          <span>Phone number</span>
+          <span>Action</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import NavBar from '@/components/NavBar.vue';
-import '../style/allusers.css'
-import SideBar from '@/components/SideBar.vue';
+import NavBar from "@/components/NavBar.vue";
+import "../style/allusers.css";
+import SideBar from "@/components/SideBar.vue";
 export default {
-    name:'AllUsers',
-    components :{NavBar,SideBar},
-    data(){
-      return{
-        title:'All Users',
-        subtitle:'All Users Information',
-        user:'Sandeep Sreekumar',
-        isSidebarActive:true
-      }
+  name: "AllUsers",
+  components: { NavBar, SideBar },
+  data() {
+    return {
+      title: "All Users",
+      subtitle: "All Users Information",
+      user: "Sandeep Sreekumar",
+      isSidebarActive: true,
+    };
+  },
+  methods: {
+    setSidebarActive() {
+      this.isSidebarActive = !this.isSidebarActive;
     },
-    methods:{
-      setSidebarActive(){
-        this.isSidebarActive=!this.isSidebarActive
-      }
-    }
-}
+  },
+};
 </script>
 
 <style>
-
 </style>
