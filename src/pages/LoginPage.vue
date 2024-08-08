13a446ee17989c7cfc7fc9f2ce85850ee62abac4
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-08-08 21:32:00 +0530

    added the animation function to the login page. awaiting further instructions

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,32 +1,32 @@
 <template>
   <div class="login-container">
     <div class="left-container">
-      <img :src="LoginImage" alt="" />
-      <span> Sed ut perspiciatis unde omnis iste natus </span>
-      <p>
+      <img :src="LoginImage" alt="" class="left"/>
+      <span class="left"> Sed ut perspiciatis unde omnis iste natus </span>
+      <p class="left">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididun.
       </p>
     </div>
     <div class="right-container">
       <div class="logo-container">
-        <img :src="Logo" />
+        <img :src="Logo" class="right" />
       </div>
-      <div class="login-title">
+      <div class="login-title right">
         Welcome back to the
         officeO community
       </div>
       <div class="login-buttons">
-        <div class="google">
+        <div class="google right">
           <img :src="google" alt="" />
           <p>Log In with Google</p>
         </div>
-        <div class="Microsoft">
+        <div class="Microsoft right">
           <img :src="Microsoft" alt="" />
           <p>Log In with Microsoft</p>
         </div>
       </div>
-      <input type="text" placeholder="Email Address" class="email-input" />
+      <input type="text" placeholder="Email Address" class="email-input right" />
       <div class="login-input-container">
         <input type="button" value="Login" @click="()=>{this.$router.replace('/new-profile')}"/>
       </div>
@@ -59,7 +59,24 @@ export default {
       this.passVisibility = !this.passVisibility;
     },
     loginAnimation(){
-      
+      gsap.to('.left',{
+        duration:1,
+        opacity:0,
+        stagger:0.3,
+        yPercent:-100
+      })
+      gsap.to('.right',{
+        duration:1,
+        stagger:0.3,
+        opacity:0,
+        yPercent:-100
+      })
+    },
+    handleLogin(){
+      this.loginAnimation()
+      setTimeout(() => {
+        this.$router.replace('/new-profile')
+      }, 1500);
     }
   },
 };
