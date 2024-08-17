0a3502413dc4a57bff1ec2fa4b1767a9162eb7d5
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-08-18 00:21:37 +0530

    made the users add page

--- src/components/PersonalInformation.vue
+++ src/components/PersonalInformation.vue
@@ -4,14 +4,23 @@
         <i class="bi bi-camera"></i>
     </div>
     <div class="personal-info-entering-container">
-        
+        <input type="text" placeholder="First Name">
+        <input type="text" placeholder="Last Name">
+        <input type="number" placeholder="Mobile Number" >
+        <input type="email" placeholder="Email address">
+    </div>
+    <div class="button-container">
+        <input type="button" value="Cancel">
+        <input type="button" value="Save" id="save">
     </div>
   </div>
 </template>
 
 <script>
 import "../style/personalinfo.css";
-export default {};
+export default {
+
+};
 </script>
 
 <style>
