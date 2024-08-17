6fd6403d7779a1a7690f7c6d9324ab80d875195e
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-08-17 22:04:56 +0530

    made the components and page for users page

---
+++ src/components/AccountAccess.vue
@@ -0,0 +1,13 @@
+<template>
+  account access
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/PersonalInformation.vue
@@ -0,0 +1,13 @@
+<template>
+  personal info
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
