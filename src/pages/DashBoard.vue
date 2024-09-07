3c9aa17fa7ecebfd9c6b3a14f274d6fe2cbca052
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-09-07 22:13:02 +0530

    made the dashboard responsive

--- src/pages/DashBoard.vue
+++ src/pages/DashBoard.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="dashBoard-container">
     <NavBar :title="title" :subtitle="subtitle" :user="user" @toggle-sidebar="setSidebarActive"/>
+  <div class="dashBoard-container">
     <div :class="['dashBoard-components',{active:isSidebarActive}]">
         <SideBar :isActive="isSidebarActive" :activeSpan="1"/>
-        DashBoard
+        <span>Dash Board</span>
     </div>
   </div>
+  <FooterComponent/>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue';
 import '../style/dashboard.css'
 import SideBar from '@/components/SideBar.vue';
+import FooterComponent from '@/components/FooterComponent.vue';
 export default {
     name:'DashBoard',
-    components:{NavBar,SideBar},
+    components:{NavBar,SideBar,FooterComponent},
     data(){
         return{
             isSidebarActive:true,
