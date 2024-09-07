bcda7ed36d6942cafacd3c606b7da1c2085d1a15
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-09-07 21:58:34 +0530

    fixed the bug on the settings page

--- src/pages/SettingsPage.vue
+++ src/pages/SettingsPage.vue
@@ -1,20 +1,22 @@
 <template>
+  <NavBar :title="title" :subtitle="subtitle" :user="user" @toggle-sidebar="setSidebarActive"/>
   <div class="settings-container">
-    <NavBar :title="title" :subtitle="subtitle" :user="user" @toggle-sidebar="setSidebarActive"/>
     <div :class="['settings-components',{active:isSidebarActive}]">
       <SideBar :isActive="isSidebarActive" :activeSpan="5"/>
-      settings
+      <span>Settings Page</span>
     </div>
   </div>
+  <FooterComponent/>
 </template>
 
 <script>
 import NavBar from "@/components/NavBar.vue";
 import '../style/settingspage.css'
 import SideBar from "@/components/SideBar.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
 export default {
   name: "SettingsPage",
-  components: { NavBar , SideBar},
+  components: { NavBar , SideBar,FooterComponent},
   data() {
     return {
       title: "Settings",
