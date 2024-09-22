324b788e154f8ca742fe4a3a9a9f6abbc12a8853
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-09-22 10:37:33 +0530

    validation completed

--- src/components/PersonalInformation.vue
+++ src/components/PersonalInformation.vue
@@ -6,21 +6,32 @@
     <div class="personal-info-entering-container">
         <div class="input-wrapper">
           <input type="text" v-model="firstname" placeholder="First Name">
+          <div :class="['error-message',{'active':fnameError}]">
+            *this field is required
+          </div>
         </div>
         <div class="input-wrapper">
           <input type="text" v-model="lastname" placeholder="Last Name">
+          <div :class="['error-message',{'active':lnameError}]">
+            *this field is required
+          </div>
         </div>
         <div class="input-wrapper">
           <input type="number" v-model="mobilenumber" placeholder="Mobile Number">
-
+          <div :class="['error-message',{'active':mobileError}]">
+            *this field is required
+          </div>
         </div>
         <div class="input-wrapper">
           <input type="email" v-model="email" placeholder="Email address">
+          <div :class="['error-message',{'active':emailError}]">
+            *this field is required
+          </div>
         </div>
       </div>
     <div class="button-container">
         <input type="button" value="Cancel">
-        <input type="button" value="Save" id="save" @click="randomResult">
+        <input type="button" value="Save" id="save" @click="checkUsers">
         <ThumbsUp v-if="status" :show="showThumbsUp" :title="userAddTitle"/>
         <ThumbsDown v-else :show="showThumbsUp" :title="userFailedTitle"/>
     </div>
@@ -42,18 +53,65 @@ export default {
       userAddTitle:'User Added..!',
       userFailedTitle:'User not Added..!',
       status:false,
+      fnameError:false,
+      lnameError:false,
+      mobileError:false,
+      emailError:false,
       firstname:'',
       lastname:'',
       email:'',
       mobilenumber:null
     }
   },
-  methods:{
+  methods: {
     addUsers(){
-      
+      // Function to add the user after validation
+      alert('pass')
     },
-    
-  }
+    checkUsers() {
+      // Validate First Name
+      if (this.firstname.length === 0) {
+        this.fnameError = true;
+        console.log('First name is required');
+      } else {
+        this.fnameError = false;
+      }
+
+      // Validate Last Name
+      if (this.lastname.length === 0) {
+        this.lnameError = true;
+      }else{
+        this.lnameError = false;
+      }
+
+      // Validate Mobile Number
+      if (!this.mobilenumber || this.mobilenumber.toString().length !== 10) {
+        this.mobileError=true
+      }else{
+        this.mobileError=false
+      }
+
+      // Validate Email
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(this.email)) {
+        this.emailError=true
+      }else{
+        this.emailError=false
+      }
+
+      // Additional logic to check for all fields being valid
+      if (
+        this.firstname.length > 0 &&
+        this.lastname.length > 0 &&
+        this.mobilenumber &&
+        this.mobilenumber.toString().length === 10 &&
+        emailRegex.test(this.email)
+      ) {
+        this.addUsers(); // Call addUsers function if validation is successful
+      }
+    }
+}
+
 };
 </script>
 
